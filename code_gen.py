2d7180b48d2a22d31e3f7b2de2d184edb16a5581
rangosta <rangosta@beagle.naist.jp>
2019-08-30 14:20:24 +0900

    modify fifo size

--- code_gen.py
+++ code_gen.py
@@ -1,16 +1,20 @@
-num = 2048
+num = 201
 
 print("    Sum2[0] <= 1 ^ Hit ^ Ci;")
 print("    Co2[0] <= (1&Hit | (1|Hit)&Ci);")
 print("\n",end="")
+'''
 
 for i in range(1, num):
     print("    Sum2[",i,"] <= 1 ^ 0 ^ Co2[",i-1,"];")
     print("    Co2[",i,"] <= (1&0 | (1|0)&Co2[",i-1,"]);")
     print("\n",end="")
 '''
-print("if (Sum2[",num-1,"]==0) Sum2_hex = ",num-1,";")
+print("if (Sum[",num-1,"]==0) Sum_hex = ",num-1,";")
 print("\n",end="")
 for i in range(1, num):
-    print("else: if (Sum2[",num-1-i,"]==0) Sum2_hex =",num-1-i,";")
+    print("else: if (Sum[",num-1-i,"]==0) Sum_hex =",num-1-i,";")
+'''
+for i in range(1, num):
+    print("adder:adder_",i,"(.Sum(Sum[",i,"]),:.Co(Co[",i,"]),:.A(1),:.B(0),:.Ci(Co[",i-1,"]));")
 '''
